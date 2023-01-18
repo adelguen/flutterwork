976b7c6be6e4579d5a509336d2bc896d8f3352a0
Adel Gnaoui <70439206+adelguen@users.noreply.github.com>
2023-01-18 10:47:23 +0100

    Widget to be changed statetype
    tantque widget stateless si changement en interne tjrs widget change pas .
    donc la changer en statefull pour devenir interactif au changement internes
    stateless sensiblke aux changements uniqyuement en externe

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  int _questionIndex=0;
   final _questions= [
     {"Question" : "What language usede with Flutter","reponses" :[]},
     {"Question" : "What is the main component in  Flutter Application ","reponses" :[]},
@@ -13,7 +14,11 @@ class MyApp extends StatelessWidget {
   ];
 
  MyApp();
-  
+void _getreponse(){
+   print (" reponse selectionnée !");
+   _questionIndex++;
+}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +27,7 @@ class MyApp extends StatelessWidget {
         body: 
       Column(
         children: [
-           Text(_questions[0]['Question'] ),
+           Text(_questions[_questionIndex]['Question'] ),
           ElevatedButton(onPressed: () => print('Reponse 1 selectionnée'), child: const Text ('reponse 1')),
           ElevatedButton(onPressed: () => print('Reponse 2 selectionnée'), child: const Text ('reponse 2')),
           ElevatedButton(onPressed: () => print('Reponse 3 selectionnée'), child: const Text ('reponse 3')),
@@ -32,5 +37,6 @@ class MyApp extends StatelessWidget {
       
     
     );
+
   }
 }
