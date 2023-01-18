3159e6008ed0e9a1dab5aad6cdd93167da843b7c
Adel Gnaoui <70439206+adelguen@users.noreply.github.com>
2023-01-18 11:09:32 +0100

    to statefull widget
    index incremented for questions buttons

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,17 +5,26 @@ void main() {
   runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  MyApp();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   int _questionIndex=0;
+
   final _questions= [
     {"Question" : "What language usede with Flutter","reponses" :[]},
     {"Question" : "What is the main component in  Flutter Application ","reponses" :[]},
     {"Question" : "When First Flutter Version where relesased ","reponses" :[]}
   ];
 
- MyApp();
 void _getreponse(){
-   print (" reponse selectionnée !");
+   print (" reponse selectionnée !") ;
+   print(_questionIndex);
    _questionIndex++;
 }
 
@@ -28,9 +37,9 @@ void _getreponse(){
       Column(
         children: [
            Text(_questions[_questionIndex]['Question'] ),
-          ElevatedButton(onPressed: () => print('Reponse 1 selectionnée'), child: const Text ('reponse 1')),
-          ElevatedButton(onPressed: () => print('Reponse 2 selectionnée'), child: const Text ('reponse 2')),
-          ElevatedButton(onPressed: () => print('Reponse 3 selectionnée'), child: const Text ('reponse 3')),
+          ElevatedButton(onPressed:  _getreponse, child: const Text ('reponse 1')),
+          ElevatedButton(onPressed: _getreponse, child: const Text ('reponse 2')),
+          ElevatedButton(onPressed: _getreponse, child: const Text ('reponse 3')),
 ],
       ),
       )
