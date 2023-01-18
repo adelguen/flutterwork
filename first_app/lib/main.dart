e313ef8b713adf26121c1f9d6eb0bb93c9f7b02c
Adel Gnaoui <70439206+adelguen@users.noreply.github.com>
2023-01-18 10:40:15 +0100

    question liste like hashmap quey value

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _questions= [
+    {"Question" : "What language usede with Flutter","reponses" :[]},
+    {"Question" : "What is the main component in  Flutter Application ","reponses" :[]},
+    {"Question" : "When First Flutter Version where relesased ","reponses" :[]}
+  ];
 
+ MyApp();
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,13 +22,11 @@ class MyApp extends StatelessWidget {
         body: 
       Column(
         children: [
-           const Text('\n Question '),
+           Text(_questions[0]['Question'] ),
           ElevatedButton(onPressed: () => print('Reponse 1 selectionnée'), child: const Text ('reponse 1')),
           ElevatedButton(onPressed: () => print('Reponse 2 selectionnée'), child: const Text ('reponse 2')),
           ElevatedButton(onPressed: () => print('Reponse 3 selectionnée'), child: const Text ('reponse 3')),
-
-
-        ],
+],
       ),
       )
       
